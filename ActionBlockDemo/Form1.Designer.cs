0a8e6cc53b7a6359bbec21fff4d48cfda18725a5
Steve Ognibene <steve.ognibene@namely.com>
2019-10-16 17:22:29 -0400

    initial commit

---
+++ ActionBlockDemo/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ActionBlockDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnQueueTask = new System.Windows.Forms.Button();
+            this.txtQueuedTasks = new System.Windows.Forms.TextBox();
+            this.txtRunningTasks = new System.Windows.Forms.TextBox();
+            this.txtCompletedTasks = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCancelWorker = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.uiTimer = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCancelledTasks = new System.Windows.Forms.TextBox();
+            this.lblConsistency = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnQueueTask
+            // 
+            this.btnQueueTask.Location = new System.Drawing.Point(46, 42);
+            this.btnQueueTask.Name = "btnQueueTask";
+            this.btnQueueTask.Size = new System.Drawing.Size(217, 54);
+            this.btnQueueTask.TabIndex = 0;
+            this.btnQueueTask.Text = "Queue Task";
+            this.btnQueueTask.UseVisualStyleBackColor = true;
+            this.btnQueueTask.Click += new System.EventHandler(this.btnQueueTask_Click);
+            // 
+            // txtQueuedTasks
+            // 
+            this.txtQueuedTasks.Location = new System.Drawing.Point(511, 42);
+            this.txtQueuedTasks.Name = "txtQueuedTasks";
+            this.txtQueuedTasks.ReadOnly = true;
+            this.txtQueuedTasks.Size = new System.Drawing.Size(100, 31);
+            this.txtQueuedTasks.TabIndex = 1;
+            this.txtQueuedTasks.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtRunningTasks
+            // 
+            this.txtRunningTasks.Location = new System.Drawing.Point(511, 81);
+            this.txtRunningTasks.Name = "txtRunningTasks";
+            this.txtRunningTasks.ReadOnly = true;
+            this.txtRunningTasks.Size = new System.Drawing.Size(100, 31);
+            this.txtRunningTasks.TabIndex = 2;
+            this.txtRunningTasks.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtCompletedTasks
+            // 
+            this.txtCompletedTasks.Location = new System.Drawing.Point(511, 118);
+            this.txtCompletedTasks.Name = "txtCompletedTasks";
+            this.txtCompletedTasks.ReadOnly = true;
+            this.txtCompletedTasks.Size = new System.Drawing.Size(100, 31);
+            this.txtCompletedTasks.TabIndex = 3;
+            this.txtCompletedTasks.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(330, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Queued Tasks";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Running Tasks";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(330, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Completed Tasks";
+            // 
+            // btnCancelWorker
+            // 
+            this.btnCancelWorker.Location = new System.Drawing.Point(46, 102);
+            this.btnCancelWorker.Name = "btnCancelWorker";
+            this.btnCancelWorker.Size = new System.Drawing.Size(217, 54);
+            this.btnCancelWorker.TabIndex = 7;
+            this.btnCancelWorker.Text = "Cancel Worker";
+            this.btnCancelWorker.UseVisualStyleBackColor = true;
+            this.btnCancelWorker.Click += new System.EventHandler(this.btnCancelWorker_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(333, 190);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(153, 25);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "Worker is {0}...";
+            // 
+            // uiTimer
+            // 
+            this.uiTimer.Tick += new System.EventHandler(this.uiTimer_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(330, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Cancelled Tasks";
+            // 
+            // txtCancelledTasks
+            // 
+            this.txtCancelledTasks.Location = new System.Drawing.Point(511, 152);
+            this.txtCancelledTasks.Name = "txtCancelledTasks";
+            this.txtCancelledTasks.ReadOnly = true;
+            this.txtCancelledTasks.Size = new System.Drawing.Size(100, 31);
+            this.txtCancelledTasks.TabIndex = 9;
+            this.txtCancelledTasks.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblConsistency
+            // 
+            this.lblConsistency.AutoSize = true;
+            this.lblConsistency.Location = new System.Drawing.Point(333, 233);
+            this.lblConsistency.Name = "lblConsistency";
+            this.lblConsistency.Size = new System.Drawing.Size(126, 25);
+            this.lblConsistency.TabIndex = 11;
+            this.lblConsistency.Text = "consistency";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 305);
+            this.Controls.Add(this.lblConsistency);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCancelledTasks);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnCancelWorker);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCompletedTasks);
+            this.Controls.Add(this.txtRunningTasks);
+            this.Controls.Add(this.txtQueuedTasks);
+            this.Controls.Add(this.btnQueueTask);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQueueTask;
+        private System.Windows.Forms.TextBox txtQueuedTasks;
+        private System.Windows.Forms.TextBox txtRunningTasks;
+        private System.Windows.Forms.TextBox txtCompletedTasks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCancelWorker;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer uiTimer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCancelledTasks;
+        private System.Windows.Forms.Label lblConsistency;
+    }
+}
+
---
+++ ActionBlockDemo/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using System.Windows.Forms;
+
+namespace ActionBlockDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQueueTask_Click(object sender, EventArgs e)
+        {
+            lock (lockObject)
+            {
+                if (workQueue is null)
+                {
+                    WorkerCancelSource = new CancellationTokenSource();
+                    workQueue = new ActionBlock<int>(async i =>
+                    {
+                        var ct = WorkerCancelSource.Token;
+                        try
+                        {
+                            Interlocked.Increment(ref activeTasks);
+                            Debug.WriteLine($"processing job {i}");
+                            await Task.Delay(3_000, ct);
+                            Interlocked.Increment(ref completedTasks);
+                            return;
+                        }
+                        catch (TaskCanceledException)
+                        {
+                            Debug.WriteLine($"Job {i} was cancelled.");
+                            // don't re-throw, or it will cause the completion Task to throw.  We would log this.
+                            Interlocked.Increment(ref cancelledTasks);
+                        }
+                        catch (Exception)
+                        {
+                            // You just want to log this and do not throw.
+                        }
+                        finally
+                        {
+                            Interlocked.Decrement(ref activeTasks);
+                        }
+                    }, new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxDegreeOfParallelism, CancellationToken = WorkerCancelSource.Token });
+                }
+                var next = Interlocked.Increment(ref nextJobId);
+                workQueue.Post(next);
+            }
+        }
+
+        public ActionBlock<int> workQueue { get; set; }
+        private int nextJobId = -1;
+        private int activeTasks = 0;
+        private int completedTasks = 0;
+        private int cancelledTasks = 0;
+        private const int maxDegreeOfParallelism = 4;
+        private const int maxQueuedTasks = 10;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            workQueue = null;
+            uiTimer.Interval = 30;
+            uiTimer.Start();
+        }
+
+        private void uiTimer_Tick(object sender, EventArgs e)
+        {
+            txtQueuedTasks.Text = workQueue?.InputCount.ToString() ?? "0";
+            txtRunningTasks.Text = activeTasks.ToString();
+            txtCompletedTasks.Text = completedTasks.ToString();
+            txtCancelledTasks.Text = cancelledTasks.ToString();
+            btnQueueTask.Enabled = workQueue is null || workQueue?.InputCount < maxQueuedTasks;
+            btnCancelWorker.Enabled = !(WorkerCancelSource is null || WorkerCancelSource.IsCancellationRequested);
+            if (workQueue is null)
+            {
+                lblStatus.Text = $"Worker is currently null.  Please queue a task.";
+            }
+            else
+            {
+                lblStatus.Text = $"Worker will process up to {maxDegreeOfParallelism} tasks at a time, with a max of {maxQueuedTasks} in the queue.";
+            }
+            var expectedNextJobId = activeTasks + completedTasks + cancelledTasks + (workQueue?.InputCount ?? 0) - 1;
+            lblConsistency.Text = expectedNextJobId == nextJobId ? "system is consistent" : $"system lost {nextJobId - expectedNextJobId} tasks.";
+        }
+
+        private CancellationTokenSource WorkerCancelSource { get; set; }
+
+        private async void btnCancelWorker_Click(object sender, EventArgs e)
+        {
+            if (workQueue is null)
+            {
+                return;
+            }
+            workQueue.Complete();
+            var queuedToCancel = workQueue.InputCount;
+            WorkerCancelSource.Cancel();
+
+            try
+            {
+                await workQueue.Completion;
+            }
+            catch (TaskCanceledException)
+            {
+                // this exception is expected.  the queued tasks won't actuall fire their own cancel event,
+                // so if we want to track them, we have to do it here.
+                Interlocked.Add(ref cancelledTasks, queuedToCancel);
+            }
+            catch (Exception)
+            {
+                // this is unexpected.
+                throw;
+            }
+            
+            workQueue = null;
+            WorkerCancelSource.Dispose();
+            WorkerCancelSource = null;
+        }
+
+        private object lockObject = new object();
+
+    }
+}
